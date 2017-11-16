38f852f14c662a976621babb86cd137abc5d67bc
Oleksandr <Oleksandr@KRAVCHUK>
2017-11-16 15:07:22 +0200

    Add project files.

---
+++ ConsoleApplication12/Source.cpp
@@ -0,0 +1,15 @@
+#include  <iostream>
+#include  <ctime>
+using namespace std;
+
+int main()
+{
+	cout << "My new project" << endl;
+	int a = clock();
+
+	cout << a << endl;
+	cout << ((float)a / CLOCKS_PER_SEC) << endl;
+	system("pause");
+
+	return  0;
+}
