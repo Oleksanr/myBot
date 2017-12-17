4e645f04c1f5b1892ab33ed9120efd56eff9d4cb
Oleksandr <Oleksandr@KRAVCHUK>
2017-12-17 19:34:58 +0200

    newInterfais and algorithm recognition

--- Win32Project1/MainDlg.cpp
+++ Win32Project1/MainDlg.cpp
@@ -1,6 +1,6 @@
 #include "MainDlg.h"
-
-
+#include <iostream>
+#include <Shellapi.h>
 
 MainDlg::MainDlg(HINSTANCE hIstanc, LPCSTR dlgResName, HWND hwndParent)
 	:DlgBox(hIstanc,dlgResName,hwndParent)
@@ -22,10 +22,27 @@ LRESULT MainDlg::RealDlgProc(HWND   hwnd, UINT   uMsg, WPARAM wParam, LPARAM lPa
 	switch (uMsg)
 	{
 	case WM_COMMAND:
-		if (wParam == IDOK )
+		if (wParam == IDOK ||wParam== IDCANCEL)
 		{
 			MessageBox(m_hWnd, "Buton is pushed!", "!!!", MB_OK);
+		
+		}
+		if (wParam== IDC_BUTTON1)
+		{
 
+			
+			/*STARTUPINFO si = { sizeof(si) };
+			PROCESS_INFORMATION pi;
+			TCHAR czCommandLine[] = "cmd bb 10 10 src.txt v.txt";
+			CreateProcess(NULL, czCommandLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+			//bb 10 10 src.txt v.txt*/
+			//system("bb 10 10 src.txt v.txt");
+			STARTUPINFO si;
+			PROCESS_INFORMATION pi;
+			ZeroMemory(&si, sizeof(STARTUPINFO));
+			CreateProcess(NULL, "bbb 10 10 src.txt v.txt", NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+			Sleep(1500);
+			
 		}
 		break;
 	}
